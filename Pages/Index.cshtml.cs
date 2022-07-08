c9d6209e4c8c23afb384f00d88c1151b48c43c3a
martinhelgesen <martin.helgesen@simployer.com>
2022-07-08 10:06:56 +0200

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace portal.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    private readonly IConfiguration _configuration;
+    public string? AppContext { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+         _configuration = configuration;
+    }
+
+    public void OnGet()
+    {
+        AppContext = _configuration["AppContext"];
+    }
+}
